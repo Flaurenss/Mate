6596a62886f973552fbdc532a1ffe6d43eb757ed
Flaurenss <lorenzonapl@hotmail.com>
2025-04-03 23:15:17 +0200

    Create base MovementSystem WIP until scripting is added

---
+++ Dobro/MovementSystem.cpp
@@ -0,0 +1,15 @@
+#include "MovementSystem.h"
+
+MovementSystem::MovementSystem()
+{
+	// TODO:
+	// RequireComponent<TransformComponent>
+}
+
+void MovementSystem::Update()
+{
+	//for (auto entity : GetEntities())
+	//{
+	//	// TODO Update position based on input?
+	//}
+}
