21620c0632ca4150691ced5b577b9c4378151a1d
Flaurenss <lorenzonapl@hotmail.com>
2025-04-04 23:35:50 +0200

    Add doc for pool concept

--- Dobro/ECS.h
+++ Dobro/ECS.h
@@ -6,9 +6,23 @@
 class ECS
 {
 public:
+	void CreateEntity();
+	void DestroyEntity();
+	void AddSystem();
+	void RemoveComponent();
+
 	void AddEntity(Entity* entity);
 	void Update(float deltaTime);
 	void Render();
 private:
-	std::vector<Entity*> entities;
+	int entities;
+	//std::vector<Entity*> entities;
 };
+
+// Pool component concept:
+//  [0]....[1] [2] [3]
+//componentID
+//   |
+//   v
+//  [0] ->EntityId n0 
+//  [1] -> EntityId n1
