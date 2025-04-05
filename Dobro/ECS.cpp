d602b591488719dc9d28a57c97ac482963c5d664
Flaurenss <lorenzonapl@hotmail.com>
2025-04-05 20:51:01 +0200

    Refactor ECS class

--- Dobro/ECS.cpp
+++ Dobro/ECS.cpp
@@ -1,5 +1,6 @@
 #include "ECS.h"
-#include "ComponentRegistry.h"
+
+int IComponent::nextId = 0;
 
 Entity ECS::CreateEntity()
 {
@@ -9,63 +10,9 @@ Entity ECS::CreateEntity()
 	return entity;
 }
 
-template<typename TComponent, typename ...TArgs>
-void ECS::AddComponent(Entity entity, TArgs && ...args)
+void ECS::AddEntityToSystem(Entity entity)
 {
-	const auto componentId = Component<TComponent>::GetId();
-	const auto entityId = entity.GetId();
 
-	// Check and resize if needed (when TComponent has not been add)
-	if (componentId >= componentsRegistry.size())
-	{
-		componentsRegistry.resize(componentId +1, nullptr);
-	}
-
-	// Check if TComponent Registry exists, ifnot it created a new one
-	if (!componentsRegistry[componentId])
-	{
-		ComponentRegistry<TComponent>* newRegistry = new ComponentRegistry<TComponent>();
-		componentsRegistry[componentId] = newRegistry;
-	}
-
-	ComponentRegistry<TComponent>* componentRegistry = ComponentRegistry<TComponent>(componentsRegistry[componentId]);
-
-	if (entityId > componentRegistry->GetSize())
-	{
-		componentRegistry->Resize(numEntities);
-	}
-
-	// Send multiple arguments to the specified new TComponent
-	TComponent newComponent(std::forward<TArgs>(args)...);
-	componentRegistry->Set(entityId, newComponent);
-	entityComponentSignatures[entityId].set(componentId);
-}
-
-template<typename TComponent>
-void ECS::RemoveComponent(Entity entity)
-{
-	const auto entityId = entity.GetId();
-	const auto componentId = Component<TComponent>::GetId();
-
-	// Put TComponent bit from entity at false:
-	entityComponentSignatures[entityId].set(componentId, false);
-}
-
-template<typename TComponent>
-bool ECS::HasComponent(Entity entity) const
-{
-	const auto entityId = entity.GetId();
-	const auto componentId = Component<TComponent>::GetId();
-	return entityComponentSignatures[entityId].test(componentId);
-}
-
-template<typename TComponent>
-TComponent& ECS::GetComponent(Entity entity) const
-{
-	const auto entityId = entity.GetId();
-	const auto componentId = Component<TComponent>::GetId();
-
-	return componentsRegistry[componentId].Get(entityId);
 }
 
 void ECS::Update()
--- Dobro/ECS.h
+++ Dobro/ECS.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include "System.h"
 #include "Entity.h"
+#include "ComponentRegistry.h"
+#include <memory>
 #include <unordered_map>
 #include <typeindex>
 #include <set>
@@ -24,12 +26,15 @@ public:
 	template <typename TComponent> bool HasComponent(Entity entity) const;
 	template <typename TComponent> TComponent& GetComponent(Entity entity) const;
 
+	/// <summary>
+	/// Add entity to system if entity components match ssytem components requirements.
+	/// </summary>
+	/// <param name="entity"></param>
 	void AddEntityToSystem(Entity entity);
-
-	void AddSystem();
-	System GetSystem();
-	/*void RemoveSystem();
-	bool HasSystem();*/
+	template <typename TSystem, typename ...TArgs> void AddSystem(TArgs&& ...args);
+	template <typename TSystem> void RemoveSystem();
+	template <typename TSystem> bool HasSystem() const;
+	template <typename TSystem> TSystem& GetSystem() const;
 	
 	/// <summary>
 	/// Add or Destroy entities based on pending set to add and pending set to destroy.
@@ -62,3 +67,95 @@ private:
 	// Map of active systems where index = system type
 	std::unordered_map<std::type_index, System*> systems;
 };
+
+template<typename TComponent, typename ...TArgs>
+void ECS::AddComponent(Entity entity, TArgs && ...args)
+{
+	const auto componentId = Component<TComponent>::GetId();
+	const auto entityId = entity.GetId();
+
+	// Check and resize if needed (when TComponent has not been add)
+	if (componentId >= componentsRegistry.size())
+	{
+		componentsRegistry.resize(componentId + 1, nullptr);
+	}
+
+	// Check if TComponent Registry exists, ifnot it created a new one
+	if (!componentsRegistry[componentId])
+	{
+		ComponentRegistry<TComponent>* newRegistry = new ComponentRegistry<TComponent>();
+		componentsRegistry[componentId] = newRegistry;
+	}
+
+	ComponentRegistry<TComponent>* componentRegistry = ComponentRegistry<TComponent>(componentsRegistry[componentId]);
+
+	if (entityId > componentRegistry->GetSize())
+	{
+		componentRegistry->Resize(numEntities);
+	}
+
+	// Send multiple arguments to the specified new TComponent
+	TComponent newComponent(std::forward<TArgs>(args)...);
+	componentRegistry->Set(entityId, newComponent);
+	entityComponentSignatures[entityId].set(componentId);
+}
+
+template<typename TComponent>
+void ECS::RemoveComponent(Entity entity)
+{
+	const auto entityId = entity.GetId();
+	const auto componentId = Component<TComponent>::GetId();
+
+	// Put TComponent bit from entity at false:
+	entityComponentSignatures[entityId].set(componentId, false);
+}
+
+template<typename TComponent>
+bool ECS::HasComponent(Entity entity) const
+{
+	const auto entityId = entity.GetId();
+	const auto componentId = Component<TComponent>::GetId();
+
+	return entityComponentSignatures[entityId].test(componentId);
+}
+
+template<typename TComponent>
+TComponent& ECS::GetComponent(Entity entity) const
+{
+	const auto entityId = entity.GetId();
+	const auto componentId = Component<TComponent>::GetId();
+
+	auto componentRegistry = std::static_pointer_cast<ComponentRegistry<TComponent>>(componentsRegistry[componentId]);
+	return componentRegistry->Get(entityId);
+}
+
+template<typename TSystem, typename ...TArgs>
+void ECS::AddSystem(TArgs && ...args)
+{
+	auto typeIndex = std::type_index(typeid(TSystem));
+	TSystem* newSystem(new TSystem(std::forward<TArgs>(args)...));
+	systems.insert(std::make_pair(typeIndex, newSystem));
+}
+
+template<typename TSystem>
+void ECS::RemoveSystem()
+{
+	auto typeIndex = std::type_index(typeid(TSystem));
+	auto system = systems.find(typeIndex);
+	systems.erase(system);
+}
+
+template<typename TSystem>
+bool ECS::HasSystem() const
+{
+	auto typeIndex = std::type_index(typeid(TSystem));
+	return systems.find(typeIndex != systems.end());
+}
+
+template<typename TSystem>
+TSystem& ECS::GetSystem() const
+{
+	auto typeIndex = std::type_index(typeid(TSystem));
+	auto system = systems.find(typeIndex);
+	return *(std::static_pointer_cast<TSystem>(system->second));
+}
