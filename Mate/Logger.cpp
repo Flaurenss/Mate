90b0ae4e4f7511ff8224c0818b04d546a2a1cf53
Flaurenss <lorenzonapl@hotmail.com>
2025-05-28 22:07:13 +0200

    Delete logger colors for style issues

--- Mate/Logger.cpp
+++ Mate/Logger.cpp
@@ -3,15 +3,15 @@
 
 void Logger::Log(const std::string& message)
 {
-	std::cout << "\x1B[32m" << message << "\033[0m" << std::endl;
+	std::cout << message << std::endl;
 }
 
 void Logger::War(const std::string& message)
 {
-	std::cout << "\x1B[93m" << message << "\033[0m" << std::endl;
+	std::cout << message << std::endl;
 }
 
 void Logger::Err(const std::string& message)
 {
-	std::cerr << "\x1B[91m" << message << "\033[0m" << std::endl;
+	std::cerr << message << std::endl;
 }
